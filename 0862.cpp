c077ea180f6e07a0ef38c2923f65c27466cfb304
Hong-Yi Lin <linhongyi830726@gmail.com>
2023-08-26 01:34:51 +0800

    Upload files

--- 0862.cpp
+++ 0862.cpp
@@ -1,6 +1,30 @@
 // First time
 /*
     Similar with problem 209 -> positive integers only
+
+    prefix sum + deque
+    T: O(n)/S: O(n)
+*/
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int ret=INT_MAX, n=nums.size();
+        vector<long long> prefix(n+1, 0);
+        for (int i=0; i<n; ++i) prefix[i+1]=prefix[i]+nums[i];
+        deque<int> dq;
+        for (int i=0; i<n+1; ++i) {
+            while (!dq.empty() && prefix[i]-prefix[dq.front()] >= k) {
+                ret=min(ret, i-dq.front());
+                dq.pop_front();
+            }
+            while (!dq.empty() && prefix[i] <= prefix[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+        }
+        return ret==INT_MAX?-1:ret;
+    }
+};
+
+/*
     prefix sum + min heap
     T: O(nlogn)/S: O(n)
 */
